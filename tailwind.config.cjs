2609ed1d4fa787075244a58581499cb5a94c9c2b
Shashank <you@example.com>
2022-10-21 00:10:09 +0530

    login icon done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors: {
         "green": "#1db954",
-        "dark": "#191414"
+        "black": "#191414",
+        "light-black": "#282828",
+        "primary": "#ffffff",
+        "secondary": "#b3b3b3",
+        "gray": "#535353"
       }
     },
   },
