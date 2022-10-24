b4760112a3e51f76bbd78d77b52e13d7b5950bfe
Shashank <you@example.com>
2022-10-24 14:51:15 +0530

    now playing section created

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -114,26 +114,49 @@ const onTrackSelection = (id, event) => {
 
 }
 
+const onPlayTrack = (event, { image, artistNames, name, duration, previewUrl, id }) => {
+    console.log(image, artistNames, name, duration, previewUrl, id);
+    //  <img id="now-playing-image" class="h-12 w-12" src="" alt="" />
+    //         <section class="flex flex-col justify-center">
+    //           <h2
+    //             id="now-playing-song"
+    //             class="te text-sm font-semibold text-primary"
+    //           >
+    //             song title
+    //           </h2>
+    //           <p id="now-playing-artists" class="text-xs">song artists</p>
+    //         </section>
+    const nowPlayingSongImage = document.querySelector("#now-playing-image");
+    const nowPlayingSongTitle = document.querySelector("#now-playing-song");
+    const nowPlayingSongArtist = document.querySelector("#now-playing-artists");
+    nowPlayingSongImage.src = image.url;
+
+    nowPlayingSongTitle.textContent = name;
+    nowPlayingSongArtist.textContent = artistNames;
+
+}
+
 //tracks loader
 const loadPlaylistTracks = ({ tracks }) => {
     const trackSections = document.querySelector("#tracks");
 
     let trackNum = 1;
     for (let trackItem of tracks.items) {
-        let { id, artists, name, album, duration_ms: duration } = trackItem.track;
+        let { id, artists, name, album, duration_ms: duration, preview_url: previewUrl } = trackItem.track;
         let track = document.createElement("section");
         track.id = id;
         track.className = " mt-2 p-1 track grid grid-cols-[50px_1fr_1fr_50px] items-center justify-items-start gap-4 rounded-md text-secondary duration-200 hover:bg-light-black"
         let image = album.images.find(img => img.height === 64);
+        let artistNames = Array.from(artists, artist => artist.name).join(", ");
         track.innerHTML = `<p  class=" relative w-full flex justify-center items-center justify-self-center "><span class="track-no">${trackNum++}</span></p>
               <section class="grid grid-flow-col gap-2 place-items-center">
                 <img class="h-10 w-10 " src="${image.url}" alt="${name}" />
                 <article class="flex flex-col gap-1 px-2 justify-center">
                   <h2 class="text-sm text-primary font-semibold line-clamp-1">${name}</h2>
-                  <p class="text-xs text-secondary line-clamp-1 ">${Array.from(artists, artist => artist.name).join(", ")}</p>
+                  <p class="text-xs text-secondary line-clamp-1 ">${artistNames}</p>
                 </article>
               </section>
-              <p class="text-sm">${album.name}</p>
+              <p class="text-sm line-clamp-1">${album.name}</p>
               <p  class="text-sm">${formatTime(duration)}</p>`;
 
         track.addEventListener("click", (event) => onTrackSelection(id, event));
@@ -145,6 +168,7 @@ const loadPlaylistTracks = ({ tracks }) => {
   <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />
 </svg>
 `
+        playButton.addEventListener("click", (event) => onPlayTrack(event, { image, artistNames, name, duration, previewUrl, id }))
         track.querySelector("p").appendChild(playButton);
         trackSections.appendChild(track);
     }
@@ -155,7 +179,7 @@ const fillContentForPlaylist = async (playlistId) => {
     const playlist = await fetchRequest(`${ENDPOINT.playlist}/${playlistId}`);
     console.log(playlist);
     const pageContent = document.querySelector("#page-content");
-    pageContent.innerHTML = `<header id="playlist-header" class="mx-8 py-2  border-b-[0.3px] border-secondary border-opacity-30 duration-100 ">
+    pageContent.innerHTML = `<header id="playlist-header" class="mx-8 py-2  border-b-[0.3px] border-secondary border-opacity-30 duration-100 z-[10] ">
             <nav class= "py-2">
               <ul
                 class=" text-sm uppercase grid grid-cols-[50px_1fr_1fr_50px] gap-4 text-secondary"
